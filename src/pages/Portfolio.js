72f63c45bb8267f95c6b35c87a3347806f3366c6
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-11-02 18:27:30 +0200

    added

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 // import motion
 import { motion } from 'framer-motion';
 // import transition
@@ -14,17 +14,35 @@ const Portfolio = () => {
       className='section bg-blue-200/20'
     >
       <div className='container mx-auto h-full relative'>
-        <div className='flex flex-col lg:flex-row h-full items-center gap-x-24 justify-start text-center lg:text-left pt-36 pb-8 overflow-x-auto lg:overflow-hidden'>
+        <div className='flex flex-col lg:flex-row h-full items-center gap-x-24 justify-start text-center lg:text-left pt-36 pb-8'>
           <div>
             <h1 className='text-[54px] lg:text-[108px] font-primary font-bold capitalize leading-[120%] tracking-[-0.05em] mb-10'>
               Portfolio
             </h1>
+            <p className='mb-12 max-w-md'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Senectus
+              <b> scelerisque viverra</b> at id aenean scelerisque. Nec eget
+              condimentum etiam leo.
+              <br />
+              <br />
+              Morbi at eget fusce feugiat volutpat et volutpat malesuada. At
+              suspendisse nisi, quam neque in leo sollicitudin.
+            </p>
+            <button className='btn mb-[30px] mx-auto lg:mx-0'>hire me</button>
           </div>
-          <div className='grid grid-cols-1 md:grid-cols-2  xl:grid-cols-2 gap-4 mb-8'>
-            <div className='w-[320px] h-[220px] bg-blue-200'>photo 1</div>
-            <div className='w-[320px] h-[220px] bg-blue-200'>photo 2</div>
-            <div className='w-[320px] h-[220px] bg-blue-200'>photo 3</div>
-            <div className='w-[320px] h-[220px] bg-blue-200'>photo 4</div>
+          <div className='grid grid-cols-2 gap-3'>
+            <div className='w-[220px] lg:w-[320px] h-[180px] lg:h-[220px] bg-blue-200'>
+              photo 1
+            </div>
+            <div className='w-[220px] lg:w-[320px] h-[180px] lg:h-[220px] bg-blue-200'>
+              photo 2
+            </div>
+            <div className='w-[220px] lg:w-[320px] h-[180px] lg:h-[220px] bg-blue-200'>
+              photo 3
+            </div>
+            <div className='w-[220px] lg:w-[320px] h-[180px] lg:h-[220px] bg-blue-200'>
+              photo 4
+            </div>
           </div>
         </div>
       </div>
