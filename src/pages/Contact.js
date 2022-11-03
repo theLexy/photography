ab7d953546f3de64101e8be6907ce99bb907ba14
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-11-03 10:23:42 +0200

    added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -17,31 +17,33 @@ const Contact = () => {
       <div className='container mx-auto h-full'>
         <div className='flex flex-col lg:flex-row h-full items-center gap-x-8 justify-start text-center lg:text-left pt-36'>
           {/* bg */}
-          <div className=' bg-[#EEF7F9] absolute bottom-0 left-0 right-0 top-72 -z-10'></div>
+          <div className='hidden lg:flex  bg-[#EEF7F9] absolute bottom-0 left-0 right-0 top-72 -z-10'></div>
           <div className='lg:flex-1 lg:pt-32'>
-            <h1 className='text-[54px] lg:text-[108px] font-primary font-bold capitalize leading-[120%] tracking-[-0.05em] mb-2'>
+            <h1 className='text-[54px] lg:text-[108px] font-primary font-bold capitalize leading-[120%] tracking-[-0.05em] mb-6'>
               Contact me
             </h1>
             <p className='mb-12'>I would love to get suggestions from you.</p>
-            <form>
+            <form className='flex flex-col gap-y-4'>
               <div className='flex gap-x-10'>
                 <input
-                  className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3'
+                  className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
                   type='text'
                   placeholder='Your name'
                 />
                 <input
-                  className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3'
+                  className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 placeholder:text-[#757879]'
                   type='text'
                   placeholder='Your e-mail address'
                 />
               </div>
               <input
-                className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 mb-12'
+                className='outline-none border-b border-b-primary h-[60px] bg-transparent font-secondary w-full pl-3 mb-12 placeholder:text-[#757879]'
                 type='text'
                 placeholder='Your message'
               />
-              <button className='btn mb-[30px] mx-auto lg:mx-0'>Send it</button>
+              <button className='btn mb-[30px] mx-auto lg:mx-0 self-start'>
+                Send it
+              </button>
             </form>
           </div>
           <div className='lg:flex-1'>
